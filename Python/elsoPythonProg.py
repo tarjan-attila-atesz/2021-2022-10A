dd743fd27adf5026a51847c8fb5ba612d243eb64
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-09-10 10:31:37 +0200

    commit

--- Python/elsoPythonProg.py
+++ Python/elsoPythonProg.py
@@ -1,1 +1,23 @@
 
+print("Hsssssss")
+print("Hello, Python!")
+print("Hello, Atesz! -__-")
+print("45")
+print("45+10")
+print(45)
+print(45+10)
+print("Egy icipici pók felmászott az ereszen,")
+print("Jött egy nyári zápor, lemosta hirtelen,")
+print("De kisütött a nap, és felszárított mindent")
+print("Így az icipici pók felmászott az ereszen.")
+
+print("Egy icipici pók felmászott az ereszen,")
+print()
+print("Jött egy nyári zápor, lemosta hirtelen,")
+print()
+print("De kisütött a nap, és felszárított mindent")
+print()
+
+print("Egy icipici pók felmászott az ereszen,\nJött egy nyári zápor, lemosta hirtelen, \nDe kisütött a nap, és felszárított mindent \nÍgy az icipici pók felmászott az ereszen.")
+
+

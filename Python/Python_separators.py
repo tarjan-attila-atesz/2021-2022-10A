1a26694be529d94bee65a1aa16ddb2cb41a08fc3
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-09-14 09:37:01 +0200

    commit

---
+++ Python/Python_separators.py
@@ -0,0 +1,6 @@
+print("Hello,\nPython!")
+print("hello","Szia","Csumi","Csá")
+print("hello","Szia","Csumi","Csá", sep=",")
+print("1969","08","16", sep="-")
+print("1969","08","16", sep="@")
+print("20"+"2")

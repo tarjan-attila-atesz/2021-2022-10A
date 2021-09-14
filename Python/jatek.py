333ca0933741cb64e09cc8c6dcd7d6e1fc132910
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-09-14 09:39:20 +0200

    commit

---
+++ Python/jatek.py
@@ -0,0 +1,16 @@
+állat = "ló"
+ló = "Ráró"
+állat = "macska"
+print(állat)
+
+gyümölcs = "alma"
+gyümölcs = "körte"
+alma = "dinnye"
+dinnye =3
+gyümölcs = alma
+print(gyümölcs)
+
+autó = "Trabant"
+autó = "Renault"
+autó = "3-5"
+print(autó)

817f7bfd0e6dd7dfd28e119d7737fcf7cfcb1be6
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-09-22 08:06:44 +0200

    commit

---
+++ Python/beszedesPython.py
@@ -0,0 +1,11 @@
+# kommentezes #
+# nev = input(" Hogy hívnak? \n")
+# print("Szia "+nev+"!")
+# print("Szia",nev,"!")
+
+bekertSzam = int(input("Kérem a számodat: "))
+print("A bekért szám: ",bekertSzam,", négyszerese: ",bekertSzam*4)
+
+#2
+
+#3

069f9ef94d1cc47817ebdeff7be323ec7b424bef
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-10-19 09:32:36 +0200

    master

---
+++ Python/while_ciklus.py
@@ -0,0 +1,13 @@
+szamlalo = 5
+
+while szamlalo !=0:
+    print("Cikluson belül vagyok értékem:",szamlalo)
+    szamlalo -=1
+print("Cikluson kívül vagyok", szamlalo)
+
+
+szam = (int(input("Addj egy számot 0-10-ig!")))
+visszaszamol = 10
+while visszaszamol !=szam:
+        visszaszamol -=1
+        print("A szam most: ", visszaszamol)

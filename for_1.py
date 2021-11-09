3a9c8030f193dcbb4123c2de2926dc84f9ebc11b
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-11-09 08:29:22 +0100

    commit

---
+++ for_1.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    print("Az i jelenlegi értéke:",i)
+print("#############################")
+
+# 2-től 10-ig a számokat írok
+for i in range(2,11):
+    print(i," ")
+
+# 2-tól 20-ig a számok, párossával
+for i in range(2,21,2):
+    print(i," ")

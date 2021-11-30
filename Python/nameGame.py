651170edc443cf3fbc99d9410ca4e5805158d4f3
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-11-30 09:15:56 +0100

    commit

---
+++ Python/nameGame.py
@@ -0,0 +1,21 @@
+maganhangzokNelkul = ""
+
+user = "atesz"
+userWord  = user.upper()
+
+
+for letter in userWord:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        maganhangzokNelkul += letter
+
+print(maganhangzokNelkul)

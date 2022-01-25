abbfebc98bb38229ab17c9595c040dab1c4bfdae
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2022-01-25 09:27:11 +0100

    master

---
+++ Python/ProgTetelGyak.py
@@ -0,0 +1,10 @@
+#Hozz létre 10-30 közötti véletlenszámokból egy 30 elemű listát! 
+#Ez a vendéglőd elmúlt hónapjának forgalma, 
+#az az ennyi vendéged volt az egyes napokon.
+
+#FA01: Határozd meg, egy hónapban összesen hány vendéged volt?
+#FA02: Volt-e olyan nap, amikor teltházad volt (tehát 30 vendég volt 1 napon)
+#FA03: Mekkora volt a legkisebb vendégszámod?
+#FA04: Mekkora volt a legnagyobb vendégszámod?
+#FA05: Hanyadik napon volt teltház a vendéglőben?
+

dbbf5ceae8d30abc4437b04980889324f2329c07
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2022-02-15 09:43:25 +0100

    master

---
+++ Python/II.felev/example_2paramFunction.py
@@ -0,0 +1,17 @@
+def message(what, number):
+    print("Enter", what, "number", number)
+
+message("telephone", 11)
+message("price", 5)
+message("number", "number")
+
+
+def intorduction(firstName, lastName):
+    print("Hello, my name is", firstName, lastName)
+
+intorduction("Luke","Skywalker")
+intorduction("Aladár","Minta")
+intorduction("Elek","Teszt")
+
+
+intorduction(firstName = "Minta", lastName = "Aladár")

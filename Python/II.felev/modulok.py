6e7e65af9b73cc6c9fc7d6208ca0188cc0e7d5e6
Tarjan Attila <tarjan.attila@sziszi.hu>
2022-04-05 08:42:47 +0200

    master

---
+++ Python/II.felev/modulok.py
@@ -0,0 +1,16 @@
+import math
+
+print(math.sin(math.pi/2))
+
+
+#----------
+pi = 3.14
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+print(sin(pi/2))
+print(math.sin(math.pi/2))

130e74c72ec0183c6cf0ed3d0f8f615053bc2d97
Tarjan Attila <tarjan.attila@sziszi.hu>
2022-04-05 09:42:10 +0200

    commit

--- Python/II.felev/ModulIras/main.py
+++ Python/II.felev/ModulIras/main.py
@@ -1,8 +1,11 @@
 from elsoModulom import sum1, prod1
-
+from sys import path
 
 zeroes = [0 for i in range(5)]
 ones = [1 for i in range(5)]
 print(sum1(zeroes))
 print(prod1(ones))
 
+#Modul betöltése a fő klönyvtárba
+
+path.append('..\\modules')

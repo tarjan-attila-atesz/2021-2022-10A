13ca6a10d55252a53ccb8a67a96bdfd9d40e218c
Tarjan Attila <tarjan.attila@sziszi.hu>
2022-05-24 08:43:42 +0200

    commit

---
+++ Python/II.felev/OOP/szuperhos.py
@@ -0,0 +1,7 @@
+class szuperhos:
+    def __init__(self,nev, szuperero = 50):
+        self.nev = nev
+        self.szupero = szuperero
+        print("Szuperhős létrehozva...")
+
+hos1 = szuperhos("Thor")

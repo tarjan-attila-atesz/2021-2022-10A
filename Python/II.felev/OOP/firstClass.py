a9e7a14e1149192842dce8df07035d10f873caf3
Tarjan Attila <tarjan.attila@sziszi.hu>
2022-05-24 08:36:07 +0200

    master

---
+++ Python/II.felev/OOP/firstClass.py
@@ -0,0 +1,9 @@
+#Rossz példa
+class OsztalyNeve:
+    def metodusNeve(self):   # a self után jönnek a "tényleges" paraméterek
+        print("Ez egy metódus a következő paraméterrel:")
+
+
+
+print(OsztalyNeve.metodusNeve)
+<function OsztalyNeve.metodusNeve at 0x000002BD5ADD57E0>
